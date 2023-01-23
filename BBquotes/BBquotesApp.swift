89e1d02a1ad544901bdeffb7a5dd70f245fdb2a3
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-23 11:49:25 +0100

    Initial Commit

---
+++ BBquotes/BBquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBquotesApp.swift
+//  BBquotes
+//
+//  Created by Aymeric Pilaert on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BBquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
